8cc2a4cc598ad119579e04ef3d8fffd086ff2029
dattran000 <tranquocquocdat@gmail.com>
2024-04-20 23:02:17 +0700

    commit all version

---
+++ src/main/java/dattran/parrent/version/ParrentApplication.java
@@ -0,0 +1,13 @@
+package dattran.parrent.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dattran/parrent/version/ParrentApplicationTests.java
@@ -0,0 +1,13 @@
+package dattran.parrent.version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
